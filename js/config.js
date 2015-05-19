3eb0f8567624a0a84ca89b489a6e1ce8b91d0653
sigcorporativo-ja <alvaro.zabala@juntadeandalucia.es>
2015-05-19 15:03:24 +0200

    versión inicial

---
+++ js/config.js
@@ -0,0 +1,16 @@
+url = "http://mapea-sigc.juntadeandalucia.es/sepim_server/api";
+urlCore = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_sanlucarlamayor/search";
+urlSuggest = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_sanlucarlamayor/suggest";
+idAplicacion = 2;
+aplicacion = null;
+pilaCategorias = [];
+coor_x = null;
+coor_y = null;
+idEntidad = null;
+datos = {
+	limit: -1,
+	offset:0
+};
+showDistance = false; //JGL - indica si mostrar la distancia o no en la búsqueda por categorías
+
+
